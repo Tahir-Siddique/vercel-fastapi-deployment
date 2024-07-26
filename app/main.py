d55d74f6fe8707b6a5cb50507f6a0f27154cc2e8
Tahir Siddique <tahirsiddiquework@gmail.com>
2024-07-26 16:40:53 +0500

    Updated endpoints

--- app/main.py
+++ app/main.py
@@ -1,37 +1,12 @@
 from time import time
 from fastapi import FastAPI, __version__
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
 from app.api.v1.endpoints import router
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(router, prefix="/api/v1")
 
-html = f"""
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>FastAPI on Vercel</title>
-        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
-    </head>
-    <body>
-        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
-            <h1>Hello from FastAPI@{__version__}</h1>
-            <ul>
-                <li><a href="/docs">/docs</a></li>
-                <li><a href="/redoc">/redoc</a></li>
-            </ul>
-            <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
-        </div>
-    </body>
-</html>
-"""
-
-@app.get("/")
-async def root():
-    return HTMLResponse(html)
-
-@app.get('/ping')
+@app.get('/')
 async def hello():
-    return {'res': 'pong', 'version': __version__, "time": time()}
+    return {'description': "This FastAPI-based microservice allows users to compare responses from OpenAI's ChatGPT and Anthropic's LLM by submitting a single prompt. The API leverages asynchronous requests for efficient response retrieval and secures access with API key authentication. It stores prompts and responses in Vercel Blob Storage and is designed for seamless deployment as a serverless function on Vercel. The project includes a comprehensive test suite to ensure reliability and maintainability."}

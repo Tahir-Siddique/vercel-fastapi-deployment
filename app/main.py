d04848a63060fcbb9de866c1481effc50540080e
Tahir Siddique <tahirsiddiquework@gmail.com>
2024-07-26 15:50:28 +0500

    Fixed bugs

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from api.v1.endpoints import router as api_router
+# from api.v1.endpoints import router as api_router
 from os import getenv
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-app.include_router(api_router, prefix="/api/v1")
+# app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/")
 async def root():
